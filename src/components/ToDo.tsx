c60ce2a366983c43c0a0f0c870e623d53fec2b58
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-10-17 17:33:43 +0900

    categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,18 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const { currentTarget: { name } } = event;
+        console.log(name)
+    }
     return (
         <li>
-            {text}
-            <button>Done</button>
-            <button>To Do</button>
-            <button>Doing</button>
+            <span>{text}</span>
+            {category !== "DOING" && <button name="DOING" onClick={onClick}>Doing</button>}
+            {category !== "TO_DO" && <button name="TO_DO" onClick={onClick}>To Do</button>}
+            {category !== "DONE" && <button name="DONE" onClick={onClick}>Done</button>}
         </li>
     )
 }
