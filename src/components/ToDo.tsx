909e1769fcf36431680f120f32375114eb0b6073
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-10-17 18:17:05 +0900

    category update

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -5,7 +5,20 @@ function ToDo({ text, category, id }: IToDo) {
     const setToDos = useSetRecoilState(toDoState);
     const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         const { currentTarget: { name } } = event;
-        console.log(name)
+
+        setToDos(oldToDos => {
+            const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+            const oldToDo = oldToDos[targetIndex];
+            const newToDo = {
+                text, id,
+                category: name as any
+            }
+            return [
+                ...oldToDos.slice(0, targetIndex),
+                newToDo,
+                ...oldToDos.slice(targetIndex + 1)
+            ];
+        })
     }
     return (
         <li>
