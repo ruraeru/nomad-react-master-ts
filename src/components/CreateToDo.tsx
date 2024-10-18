0b707b0ad133a6b985c73aa0df10807038724240
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-10-18 16:45:27 +0900

    enum

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
-import { useSetRecoilState } from "recoil";
-import { toDoState } from "../atoms";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { categoryState, toDoState } from "../atoms";
 
 interface IForm {
     toDo: string;
@@ -8,14 +8,15 @@ interface IForm {
 
 function CreateToDo() {
     const setToDos = useSetRecoilState(toDoState);
+    const category = useRecoilValue(categoryState);
     const { register, handleSubmit, setValue } = useForm<IForm>();
     const handleValid = ({ toDo }: IForm) => {
         console.log("add todo", toDo);
-        setToDos(prev => [{
+        setToDos((oldToDos) => [{
             text: toDo,
             id: Date.now(),
-            category: "TO_DO"
-        }, ...prev]);
+            category
+        }, ...oldToDos]);
         setValue("toDo", "");
     }
     return (
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,36 +1,28 @@
-import { useRecoilValue } from "recoil";
-import { toDoSelector, toDoState } from "../atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { Categories, categoryState, toDoSelector } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-    const [toDo, doing, done] = useRecoilValue(toDoSelector);
-
+    const toDos = useRecoilValue(toDoSelector);
+    const [category, setCategory] = useRecoilState(categoryState);
+    const onInput = (e: React.FormEvent<HTMLSelectElement>) => {
+        setCategory(e.currentTarget.value as any);
+    }
+    console.log(toDos)
     return (
         <div>
             <h1>To Dos</h1>
             <hr />
+            <select value={category} onInput={onInput}>
+                <option value={Categories.TO_DO}>To Do</option>
+                <option value={Categories.DOING}>Doing</option>
+                <option value={Categories.DONE}>Done</option>
+            </select>
             <CreateToDo />
-            <h2>To Do</h2>
-            <ul>
-                {toDo.map((toDo) => (
-                    <ToDo key={toDo.id} {...toDo} />
-                ))}
-            </ul>
-            <hr />
-            <h2>Doings</h2>
-            <ul>
-                {doing.map((toDo) => (
-                    <ToDo key={toDo.id} {...toDo} />
-                ))}
-            </ul>
-            <hr />
-            <h2>Done</h2>
-            <ul>
-                {done.map((toDo) => (
-                    <ToDo key={toDo.id} {...toDo} />
-                ))}
-            </ul>
+            {toDos?.map((toDo) => (
+                <ToDo key={toDo.id} {...toDo} />
+            ))}
         </div>
     );
 }
