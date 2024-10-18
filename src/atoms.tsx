c355e8b235c1118fc312c815035d56732430033f
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-10-18 11:25:37 +0900

    recoill selector

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,12 +1,24 @@
-import { atom } from "recoil";
-
-export const toDoState = atom<IToDo[]>({
-    key: "toDo",
-    default: []
-});
+import { atom, selector } from "recoil";
 
 export interface IToDo {
     text: string;
     id: number;
     category: "DONE" | "DOING" | "TO_DO";
 }
+
+export const toDoState = atom<IToDo[]>({
+    key: "toDo",
+    default: [],
+});
+
+export const toDoSelector = selector({
+    key: "toDoSelector",
+    get: ({ get }) => {
+        const toDos = get(toDoState);
+        return [
+            toDos.filter((toDo) => toDo.category === "TO_DO"),
+            toDos.filter((toDo) => toDo.category === "DOING"),
+            toDos.filter((toDo) => toDo.category === "DONE"),
+        ];
+    }
+})
