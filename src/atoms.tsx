fedcdb65403f21f48c87a76785fce989ef9798d0
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-10-19 12:15:09 +0900

    todo saved localstorage

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,5 +1,18 @@
 import { atom, selector } from "recoil";
 
+const localStorageEffect = (key: string) => ({ setSelf, onSet }: any) => {
+    const savedValue = localStorage.getItem(key)
+    if (savedValue != null) {
+        setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue: any, isReset: any) => {
+        isReset
+            ? localStorage.removeItem(key)
+            : localStorage.setItem(key, JSON.stringify(newValue))
+    });
+};
+
 export enum Categories {
     "TO_DO" = "TO_DO",
     "DOING" = "DOING",
@@ -20,6 +33,9 @@ export const categoryState = atom<Categories>({
 export const toDoState = atom<IToDo[]>({
     key: "toDo",
     default: [],
+    effects: [
+        localStorageEffect("toDo")
+    ]
 });
 
 export const toDoSelector = selector({
