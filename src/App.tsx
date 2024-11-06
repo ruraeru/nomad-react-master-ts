1fa40a98d216dc3b4d87719189aaf8f3f95cfb62
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-11-06 12:47:19 +0900

    Header part one

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./components/Header";
+
 export default function App() {
-	return null;
+	return (
+		<Router>
+			<Header />
+			<Switch>
+				<Route path="/">
+					<Home />
+				</Route>
+				<Route path="/tv">
+					<Tv />
+				</Route>
+				<Route path="/search">
+					<Search />
+				</Route>
+			</Switch>
+		</Router>
+	)
 }
