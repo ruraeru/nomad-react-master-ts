c362c5c8e6b889db5956c280a46e34820b61fcbd
xodn_ghkd <hwa1023@mjc.ac.kr>
2024-11-07 10:11:09 +0900

    #9.10 Box Animations part Two

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -56,14 +56,28 @@ const Box = styled(motion.div) <{ $bgPhoto: string }>`
     background-size: cover;
     background-position: center;
     height: 200px;
-
     font-size: 66px;
+
     &:first-child {
         transform-origin: center left;
     }
 
     &:last-child {
         transform-origin: center right;
+    }
+`;
+
+const Info = styled(motion.div)`
+    padding: 10px;
+    background-color: ${props => props.theme.black.lighter};
+    opacity: 0;
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+
+    h4 {
+        text-align: center;
+        font-size: 18px;
     }
 `;
 
@@ -87,7 +101,19 @@ const BoxVariants = {
         scale: 1.3,
         y: -50,
         transition: {
-            delay: 0.3,
+            delay: 0.5,
+            duration: 0.3,
+            type: "tween"
+        }
+    }
+}
+
+const infoVariants = {
+    hover: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duration: 0.3,
             type: "tween"
         }
     }
@@ -140,7 +166,11 @@ export default function Home() {
                                                 type: "tween"
                                             }}
                                             $bgPhoto={makeImagePath(movie.backdrop_path || "")}
-                                        />
+                                        >
+                                            <Info variants={infoVariants}>
+                                                <h4>{movie.title}</h4>
+                                            </Info>
+                                        </Box>
                                     ))}
                             </Row>
                         </AnimatePresence>
